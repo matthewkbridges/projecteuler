48711d71536ae19ce05b880aa43f27ffba8d6af1
matt <matt@matthewbridges.com>
2022-11-04 23:09:03 -0400

    Use combinatorics to determine paired primes under the square root of the number being tested.

--- src/Main.java
+++ src/Main.java
@@ -4,33 +4,44 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
         int numberToTest = 30;
-        List<Integer> primes = findPrimesLessThanN(numberToTest);
+        List<Integer> primes = new ArrayList<>();
+
+        int indexOfSquareRoot = findPrimesLessThanN(numberToTest, primes);
         System.out.println("Primes:");
         for(int i : primes) {
             System.out.println(i);
         }
-        int root = (int) Math.sqrt( numberToTest);
-        System.out.println("Root: " + root);
 
+        int numberOfTwoFactors = 0;
         for(int i=0;i<primes.size();i++) {
-            for(int j=i;j<primes.size();j++) {
+            for(int j=indexOfSquareRoot+1;j<primes.size();j++) {
                 int testNumber = primes.get(i) * primes.get(j);
                 if(testNumber < numberToTest) {
                     System.out.println("Two Factor Composite: " + testNumber);
+                    numberOfTwoFactors++;
                 }
             }
         }
+        long twoFactorCompositesLessThanSqrt = (factorial(indexOfSquareRoot+1) / (factorial(indexOfSquareRoot-1) * 2L)) + indexOfSquareRoot+1L;
+        System.out.println("Final count of two factors: " + (numberOfTwoFactors + twoFactorCompositesLessThanSqrt));
     }
 
     //TODO: Switch to a sieve
-    private static List<Integer> findPrimesLessThanN(int n) {
-        List<Integer> retVal = new ArrayList<>();
+    private static int findPrimesLessThanN(int n, List<Integer> retVal) {
+        int squareRootIndex = -1;
+        int root = (int) Math.sqrt( n);
+        System.out.println("Root: " + root);
+
         for(int i=2; i<n; i++) {
             if(isPrime(i)) {
                 retVal.add(i);
+                if(i<=root) {
+                    squareRootIndex++;
+                }
             }
         }
-        return retVal;
+        System.out.println("largest index >= square root of test number: " + squareRootIndex);
+        return squareRootIndex;
     }
 
     //TODO: Naive implementation, fix this
@@ -41,5 +52,13 @@ public class Main {
             }
         }
         return true;
+    }
+
+    private static long factorial(int n) {
+        long retVal = 1;
+        for(int i=2; i<=n; i++) {
+            retVal = retVal * i;
+        }
+        return retVal;
     }
 }
