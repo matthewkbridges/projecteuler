f4b74c277b3e843d14da79c22aba89cb30ac5403
matt <matt@matthewbridges.com>
2024-02-15 20:12:59 -0500

    Move loop limits out of the loop construct so they are executed once.  Use bit-shifting instead of division by 2.  Get rid of a shift for the bit-index and expand the mask lookup array.

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
-    private static final int[] bitIndexes= {0x1, 0x2, 0x4, 0x8, 0x10, 0x20, 0x40, 0x80};
+    private static final int[] bitIndexes= {0x0, 0x1, 0x0, 0x2, 0x0, 0x4, 0x0, 0x8, 0x0, 0x10, 0x0, 0x20, 0x0, 0x40, 0x0, 0x80};
 
     public static void main(String[] args) {
         long start = System.currentTimeMillis();
@@ -10,8 +10,9 @@ public class Main {
         int numberToTest = 100000000;
         List<Long> primes = findPrimesLessThanNSieve(numberToTest);
         int numberOfTwoFactors = 0;
-        for(int i=0;i<primes.size();i++) {
-            for(int j=i;j<primes.size();j++) {
+        int lim=primes.size();
+        for(int i=0;i<lim;i++) {
+            for(int j=i;j<lim;j++) {
                 long testNumber = primes.get(i) * primes.get(j);
                 if(testNumber < numberToTest) {
                     numberOfTwoFactors++;
@@ -35,15 +36,17 @@ public class Main {
         int i;
         int j;
         int k;
+        int ilim=(n>>1)+1;
         int kind;
 
-        for(i=3; i<=n/2; i=i+2) {
-            if((isComposite[i>>4] & bitIndexes[(i&0xF)>>1]) == 0) {
+        for(i=3; i<ilim; i=i+2) {
+            if((isComposite[i>>4] & bitIndexes[i&0xF]) == 0) {
                 retVal.add((long)i);
-                for(j=3;j<=n/i;j=j+2) {
+                int jlim=(n/i)+1;
+                for(j=3;j<jlim;j=j+2) {
                     k=j*i;
                     kind=k>>4;
-                    isComposite[kind]=(byte) (isComposite[kind] | bitIndexes[(k&0xF)>>1]);
+                    isComposite[kind]=(byte) (isComposite[kind] | bitIndexes[k&0xF]);
                 }
             }
         }
